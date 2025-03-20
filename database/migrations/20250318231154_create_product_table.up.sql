d240a8747a0e03a3c13fcb5e06f53c82dc8b0f09
breno-ca <breno_de_avila@outlook.com>
2025-03-20 17:47:45 -0300

    fix: added missing commas in create product table migration

--- database/migrations/20250318231154_create_product_table.up.sql
+++ database/migrations/20250318231154_create_product_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS product (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
-	price DECIMAL(6, 2) NOT NULL
-	available BOOLEAN
+	price DECIMAL(6, 2) NOT NULL,
+	available BOOLEAN,
 	created TIMESTAMP DEFAULT NOW()
 );
