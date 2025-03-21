7461094892fbbad00181a4dde4af400c63617e4a
breno-ca <breno_de_avila@outlook.com>
2025-03-21 15:08:02 -0300

    feat: implemented product routes and server

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"studies-postgres-golang/config"
 	"studies-postgres-golang/database"
+
+	repository "studies-postgres-golang/repository/product"
+	service "studies-postgres-golang/service/product"
 )
 
 var (
 	cfg  = config.NewConfig().WithPostgres()
 	pgDB = database.NewPostgres(cfg)
+
+	productRepository = repository.NewProductRepository(pgDB.DB)
+	productService    = service.NewProductService(productRepository)
 )
 
 func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.DefaultServeMux)
+
+	mux.Handle("POST /v1/products", http.HandlerFunc(productService.Insert))
+	mux.Handle("GET /v1/products", http.HandlerFunc(productService.Index))
+
+	log.Println("Server running on", cfg.ServerPort)
+	if err := http.ListenAndServe(cfg.ServerPort, mux); err != nil {
+		log.Println(err.Error())
+	}
 }
